77ea6c82728b8d14c45c95300c76f347b699bb46
vigimani <victor.gillibert@gmail.com>
2022-12-08 16:39:13 +0100

    deploymenton goerli network

--- ethereum/truffle/truffle-config.js
+++ ethereum/truffle/truffle-config.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+
 module.exports = {
   networks: {
     development: {
@@ -5,6 +8,10 @@ module.exports = {
      port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
+    goerli: {
+      provider: function(){return new HDWalletProvider({mnemonic:{phrase:`${process.env.MNEMONIC}`}, providerOrUrl: `https://goerli.infura.io/v3/${process.env.INFURA_ID}`})},
+      network_id: 5, 
+    }
   },
 
   mocha: {
